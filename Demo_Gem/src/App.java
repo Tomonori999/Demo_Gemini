7cbf1e01f61fc74ab7331d5f21aefd39b586afaa
adachitm0923 <adachitm0923@gmail.com>
2025-11-23 13:17:38 +0900

    メインメソッドを修正し、引数に応じた挨拶メッセージを出力するように変更

--- Demo_Gem/src/App.java
+++ Demo_Gem/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String name;
+
+        // 💡 推論: args.length > 0 で、実行時に引数が渡されたかをチェックします。
+        if (args.length > 0) {
+            // 最初の引数 (args[0]) を名前として使用します。
+            name = args[0];
+        } else {
+            // 引数が渡されなかった場合のデフォルトの名前を設定します。
+            name = "皆さん"; 
+        }
+
+        // 挨拶メッセージを構築して出力します。
+        System.out.println(name + "さん、こんにちは！");
     }
 }
